51d30d8167ef03175a4acbb002f6708b63de031b
azcoders_gl <azcoders.gl@gmail.com>
2021-09-10 07:42:52 +0600

    class

--- php-learning/index.php
+++ php-learning/index.php
@@ -1,14 +1,37 @@
 <?php
 
 
-$task = [
-    'title' => 'Task of the day',
-    'due'   => 'Tomorrow',
-    'assigned' => 'jefry',
-    'completed' => true
-];
+require 'function.php';
 
 
+
+class Task {
+
+    protected $description;
+    protected $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function completed()
+    {
+        return $this->completed;
+    }
+
+    public function isCompleted()
+    {
+        $this->completed = true;
+    }
+}
+
+$task = new Task('Go to store');
+$task->isCompleted();
+dd($task->completed());
+
+var_dump($task);
+die;
 require 'index.view.php';
 
 
